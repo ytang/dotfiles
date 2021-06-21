f448fff3b8f23c05c90304137dc1ad72e8a2f1fe
Yang Tang <tangyang.cn@gmail.com>
2021-06-20 22:31:34 -0400

    Enable italic comments in Doom Emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,7 +21,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "PragmataPro Liga" :size 16))
+(setq doom-font (font-spec :family "OperatorMonoSSmLig Nerd Font" :size 14 :weight 'semi-light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -62,3 +62,7 @@
 
 ;; Set theme
 (setq doom-theme 'doom-monokai-pro)
+
+;; Italic comments
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic))
