1f4b5a088481bfc4457ee3eac4ec43f8745784c7
Yang Tang <tangyang.cn@gmail.com>
2021-08-29 21:31:58 -0400

    Emacs: use LLVM coding style

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -72,3 +72,24 @@
 
 ;; Italic comments.
 (custom-set-faces! '(font-lock-comment-face :slant italic))
+
+;; LLVM coding style guidelines in emacs.
+(defun llvm-lineup-statement (langelem)
+  (let ((in-assign (c-lineup-assignments langelem)))
+    (if (not in-assign)
+        '++
+      (aset in-assign 0
+            (+ (aref in-assign 0)
+               (* 2 c-basic-offset)))
+      in-assign)))
+(c-add-style "llvm.org"
+             '("gnu"
+               (fill-column . 80)
+               (c++-indent-level . 2)
+               (c-basic-offset . 2)
+               (indent-tabs-mode . nil)
+               (c-offsets-alist . ((arglist-intro . ++)
+                                   (innamespace . 0)
+                                   (member-init-intro . ++)
+                                   (statement-cont . llvm-lineup-statement)))))
+(add-hook 'c-mode-common-hook (lambda () (c-set-style "llvm.org")))
