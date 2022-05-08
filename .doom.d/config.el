9676b2bec5b0bc8bef8ca814ad27d02876839e88
Yang Tang <tangyang.cn@gmail.com>
2022-05-08 03:10:22 -0400

    Tweak Doom Emacs settings

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,12 +21,13 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "OperatorMonoSSmLig Nerd Font" :size 14 :weight 'semi-light))
+(setq doom-font (font-spec :family "Comic Code Ligatures" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-one)
+(setq doom-theme 'doom-one-light)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -62,9 +63,6 @@
 
 ;; Don't ask for confirmation when leaving Emacs.
 (setq confirm-kill-emacs nil)
-
-;; Set theme.
-(setq doom-theme 'doom-one-light)
 
 ;; New splits will be at the bottom or to the right side of the screen.
 (setq evil-split-window-below t)
